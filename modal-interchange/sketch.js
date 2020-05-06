902069321068aa69272ce7d219e3b0b49ff1baa5
Remy Hunter <remyhunter256@gmail.com>
2020-05-06 16:23:29 -0400

    Add Arpeggios

--- modal-interchange/sketch.js
+++ modal-interchange/sketch.js
@@ -1,5 +1,34 @@
 
+
+var tones = [60, 62, 64, 66];
+
+
 function setup() {
-  createCanvas(400, 400);
-  fill(100);
+  createCanvas(800, 500);
+  w=windowWidth;
+  h=windowHeight;
+
+  background(200);
+    var container = createDiv('');
+  container.position(200,200);
+
+
+  toneFilter = new p5.LowPass();
+  toneFilter.freq(700);
+
+  toneOscillator = new p5.Oscillator();
+  toneOscillator.setType('square');
+  toneOscillator.disconnect();
+  toneOscillator.connect(toneFilter);
+  toneOscillator.freq(midiToFreq(60));
+  toneOscillator.amp(0.1);
+}
+
+function keyReleased() {
+	if (key === ' '){
+		toneOscillator.start();
+	}
+}
+function draw () {
+
 }
