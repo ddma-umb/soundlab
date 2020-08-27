63b2d553b087d3d06a3744b9c9d9e4375d3fb730
Remy Hunter <remyhunter256@gmail.com>
2020-08-27 11:23:53 -0400

    Remove broken projects

--- vocoder/sketch.js
+++ vocoder/sketch.js
@@ -1,6 +1,37 @@
+var mic;
+var toneOscillator;
+
 function setup(){
+    toneOscillator = initializeOsc();
+    mic = new p5.AudioIn();
+    mic.start();
+    generateTone(60);
+}
+
+function draw(){
+  text('got here');
+  var vol = mic.getLevel();
+  vol = int(map(vol, 0, 1, 0, 12));
+  let midiVal = vol+60;
+  generateTone(midiVal);
+  console.log(midiVal);
 
 }
-function draw(){
+
+function initializeOsc(){
+    var toneFilter = new p5.LowPass();
+    toneOscillator = new p5.Oscillator(); 
+    toneFilter.freq(700);
+    toneOscillator.setType('square');
+    toneOscillator.disconnect();
+    toneOscillator.connect(toneFilter);
+    toneOscillator.amp(0.1);
+    return toneOscillator;  
+  }
+  function generateTone(midiVal) {
+    let freq = midiToFreq(midiVal);
+    // console.log(freq);
+    toneOscillator.freq(freq);
+    // setTimeout(generateTone, 1000);
+  }
   
-}
